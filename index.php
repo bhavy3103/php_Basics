cbc03a70f1a136a42e79121fbd134c022a7b0318
Bhavykumar Devani <105499961+bhavy3103@users.noreply.github.com>
2023-08-10 09:16:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -36,4 +36,66 @@ if(isset($_POST['name'])){
     $connection->close();
 }
 ?>
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to travel Form</title>
+    <!-- <link rel="preconnect" href="https://fonts.googleapis.com"> -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Borel&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <img class="bg" src="bg.jpeg" alt="Vgec">
+
+    <div class="container">
+        <h2>Welcome to Vgec trip Form</h2>
+
+        <p>Enter your details and submit to confirm your participation </p>
+
+        <?php
+        if($insert=true){
+            echo '<p class="submitMsg">Thanks for submitting your details</p>';
+        } 
+    ?>
+
+        <form action="index.php" method="post">
+
+            <input type="text" name="name" id="name" placeholder="Enter your name">
+
+            <input type="text" name="age" id="age" placeholder="Enter your age">
+
+            <input type="text" name="gender" id="gender" placeholder="Enter your gender">
+
+            <input type="email" name="email" id="email" placeholder="Enter your email">
+
+            <input type="phone" name="phone" id="phone" placeholder="Enter your phone">
+
+            <textarea name="Description" id="Description" cols="50" rows="10"
+                placeholder="Enter any other information here">
+            </textarea>
+
+
+            <button class="btn">Submit</button>
+            <!-- <button class="btn">Reset</button> -->
+
+
+
+        </form>
+
+
+    </div>
+
+    <script src="index.js"></script>
+
+    <!-- INSERT INTO `trip` (`sno`, `name`, `age`, `gender`, `email`, `phone`, `other`, `dt1`) VALUES ('1', 'bhavy', '19', 'male', 'bhavy@gmail.com', '9999999999', 'Hello ', current_timestamp()); -->
+</body>
+
+</html>
 
